4a0dce5b4c8265fcc7a24fe0e029429707bc1219
GANJI SRIVANI <110708151+srivaniganji@users.noreply.github.com>
2025-12-05 17:02:09 +0530

    Update Skills.js

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,25 +1,30 @@
 import React from 'react'; 
+
 import { CgCPlusPlus } from "react-icons/cg";
-import {FaReact, FaPython, FaGitAlt, FaGithub, FaNpm, FaFigma, FaBootstrap} from "react-icons/fa";
-import {DiNodejs, DiJavascript1} from "react-icons/di";
-import {SiExpress, SiMongodb, SiPostman, SiVercel} from "react-icons/si";
+import { FaReact, FaPython, FaGitAlt, FaGithub, FaNpm, FaFigma, FaBootstrap } from "react-icons/fa";
+import { DiNodejs, DiJavascript1 } from "react-icons/di";
+import { SiExpress, SiMongodb, SiPostman, SiVercel, SiMysql, SiFlutter, SiRender } from "react-icons/si";
+
 
 const Skills = ({skill}) => {
     const icon = {
-        'C++': <CgCPlusPlus/>,
-        Postman: <SiPostman/>,
-        React: <FaReact/>,
-        Javascript: <DiJavascript1/>,
-        Node : <DiNodejs/>,
-        Express : <SiExpress/>,
-        MongoDb : <SiMongodb/>,
-        Git : <FaGitAlt/>,
-        Github : <FaGithub/>,
-        Npm : <FaNpm/>,
-        Figma : <FaFigma/>,
-        Bootstrap: <FaBootstrap/>,
-        Vercel : <SiVercel/>
-    }
+  'C++': <CgCPlusPlus />,
+  Postman: <SiPostman />,
+  React: <FaReact />,
+  Javascript: <DiJavascript1 />,
+  Node: <DiNodejs />,
+  Express: <SiExpress />,
+  SQL: <SiMysql />,   
+  MongoDb: <SiMongodb />,
+  Git: <FaGitAlt />,
+  Github: <FaGithub />,
+  Npm: <FaNpm />,
+  Figma: <FaFigma />,
+  Bootstrap: <FaBootstrap />,
+  Vercel: <SiVercel />,
+  Render: <SiRender />,    
+  Flutter: <SiFlutter />  
+}
     
   return (
     <div title={skill} className='SkillBox'>
